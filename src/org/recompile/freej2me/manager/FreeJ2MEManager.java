dfa82e79e6f127952a71698b8979c6fa0741fe04
AbyssalAscendantAsperger <ngan987654321bach@gmail.com>
2026-06-24 16:39:59 +0700

    v2.9 Fix typo in maxConcurrentSessions initialization

--- src/org/recompile/freej2me/manager/FreeJ2MEManager.java
+++ src/org/recompile/freej2me/manager/FreeJ2MEManager.java
@@ -34,7 +34,7 @@ public class FreeJ2MEManager
 	private final ClassLoader parentLoader;
 	private final URL coreJarUrl;
 
-	private volatile int maxConcurrentSessions = 1000s;
+	private volatile int maxConcurrentSessions = 1000;
 	private volatile long sessionTimeoutMs = 300_000L; // 5 phút
 	private volatile boolean daemonRunning = true;
 
